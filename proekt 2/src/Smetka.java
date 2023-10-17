7cb2a9617c821971550e83165fa98dd6c7dcf53b
davidslave <148117374+davidslave@users.noreply.github.com>
2023-10-17 17:43:30 +0200

    Add files via upload

---
+++ proekt 2/src/Smetka.java
@@ -0,0 +1,14 @@
+
+public class Smetka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int jab=30;
+int komp=20;
+double pra=30;
+double x=(jab*2)+(komp*5)+(pra*2.5);
+System.out.println("smetka");
+System.out.println(x);
+	}
+
+}
---
+++ proekt_1/src/menuvacnica.java
@@ -0,0 +1,13 @@
+
+public class menuvacnica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int denari=2450;
+double evro=61.7;
+double x=denari/evro;
+System.out.println("evrovodenari");
+System.out.println(x);
+	}
+
+}
